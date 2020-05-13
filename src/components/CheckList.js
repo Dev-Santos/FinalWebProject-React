d04957bb16e1e716649d2e2441181705437e9669
Adrian Santo <adriansanto21@hotmail.com>
2020-05-13 07:57:27 -0600

    created react application which pulls recipes from an API

--- src/components/CheckList.js
+++ src/components/CheckList.js
@@ -60,6 +60,7 @@ class CheckList extends React.Component {
         });
     };
 
+    //function used to remove a note from the list
     removeNote(id){
         console.log("You clicked me...", id);
         if(window.confirm("Are you sure about deleting this item?")){
@@ -72,9 +73,9 @@ class CheckList extends React.Component {
                 console.log("Error deleting...", error);
             })
         }
-    }
-    
+    }    
 
+    //function used to add notes to the checklist
     submit = (event) => {
         event.preventDefault();
 
@@ -119,11 +120,8 @@ class CheckList extends React.Component {
                             value= {this.state.body}
                             onChange={this.handleChange}
                         />
-                        {/* <input type="text" placeholder="Enter item: "
-                        value={this.state.currentItem.text} onChange={this.handleInput} /> */}
                         <button type="submit">Add Item</button>
                     </form>
-                    {/* <ListItems notes={this.state.notes} /> */}
                     <div>
                         <FlipMove duration={300} easing="ease-in-out">
                             <div className="notes">
@@ -131,9 +129,6 @@ class CheckList extends React.Component {
                             </div>
                         </FlipMove>            
                     </div>
-                    {/* <ListItems items = {this.state.items}
-                    deleteItem = {this.deleteItem}
-                    setUpdate = {this.setUpdate}></ListItems> */}
                 </header>
                 
             </div>
@@ -142,60 +137,3 @@ class CheckList extends React.Component {
 }
 
 export default CheckList;
-
-
-
-    // constructor(props){
-    //     super(props);
-    //     this.state={
-    //         items:[],
-    //         currentItem:{
-    //             text:'',
-    //             key:''
-    //         }            
-    //     }
-    //     this.handleInput = this.handleInput.bind(this);
-    //     this.addItem = this.addItem.bind(this);
-    //     this.deleteItem = this.deleteItem.bind(this);
-    //     this.setUpdate = this.setUpdate.bind(this);
-    // }
-    // handleInput(e){
-    //     this.setState({
-    //         currentItem:{
-    //             text: e.target.value,
-    //             key: Date.now()
-    //         }
-    //     })
-    // }
-    // addItem(e){
-    //     e.preventDefault();
-    //     const newItem = this.state.currentItem;
-    //     console.log(newItem);
-    //     if(newItem.text!==""){
-    //         const newItems=[...this.state.items, newItem];
-    //         this.setState({
-    //             items:newItems,
-    //             currentItem:{
-    //                 text:'',
-    //                 key:''
-    //             }
-    //         })
-    //     }
-    // }
-    // deleteItem(key){
-    //     const filteredItems = this.state.items.filter( item => item.key!==key);
-    //     this.setState({
-    //         items:filteredItems
-    //     })
-    // }
-    // setUpdate(text, key){
-    //     const items = this.state.items;
-    //     items.map(item =>{
-    //         if(item.key===key){
-    //             item.text=text;
-    //         }
-    //     })
-    //     this.setState({
-    //         items: items
-    //     })
-    // }
