ea6266fed76895cd1e5a15e72b8715f11449af95
Adrian Santo <adriansanto21@hotmail.com>
2020-05-15 22:09:46 -0600

    Updates

---
+++ precache-manifest.f7a746c63aae3b80366d7a5f4b96c7fb.js
@@ -0,0 +1,50 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3efb20fdc0c3bc4ca422dcd3d941dafb",
+    "url": "/react-application/index.html"
+  },
+  {
+    "revision": "2dd59eb7073e7da001c8",
+    "url": "/react-application/static/css/2.a3565c54.chunk.css"
+  },
+  {
+    "revision": "d5e3b06af86471177fb9",
+    "url": "/react-application/static/css/main.3512d7c8.chunk.css"
+  },
+  {
+    "revision": "2dd59eb7073e7da001c8",
+    "url": "/react-application/static/js/2.31baaa75.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/react-application/static/js/2.31baaa75.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d5e3b06af86471177fb9",
+    "url": "/react-application/static/js/main.83bde9b7.chunk.js"
+  },
+  {
+    "revision": "ea222229a5a11235804c",
+    "url": "/react-application/static/js/runtime-main.dcc854ce.js"
+  },
+  {
+    "revision": "674f50d287a8c48dc19ba404d20fe713",
+    "url": "/react-application/static/media/fontawesome-webfont.674f50d2.eot"
+  },
+  {
+    "revision": "912ec66d7572ff821749319396470bde",
+    "url": "/react-application/static/media/fontawesome-webfont.912ec66d.svg"
+  },
+  {
+    "revision": "af7ae505a9eed503f8b8e6982036873e",
+    "url": "/react-application/static/media/fontawesome-webfont.af7ae505.woff2"
+  },
+  {
+    "revision": "b06871f281fee6b241d60582ae9369b9",
+    "url": "/react-application/static/media/fontawesome-webfont.b06871f2.ttf"
+  },
+  {
+    "revision": "fee66e712a8a08eef5805a46892932ad",
+    "url": "/react-application/static/media/fontawesome-webfont.fee66e71.woff"
+  }
+]);
